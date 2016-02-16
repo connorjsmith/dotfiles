2d5deea6609671e4f35162aadb5e54cf47cd80f0
Connor J Smith <connorjsmith101@gmail.com>
2016-02-16 09:38:59 -0500

    Added ps -aux alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,18 @@
 set -x EDITOR "emacsclient -t"
+set -U SXHKD_SHELL /bin/bash
+set -x XDG_CONFIG_HOME "/home/connor/.config"
+
+# Colorful man pages
+# from http://pastie.org/pastes/206041/text
+setenv -x LESS_TERMCAP_mb (set_color -o red)
+setenv -x LESS_TERMCAP_md (set_color -o red)
+setenv -x LESS_TERMCAP_me (set_color normal)
+setenv -x LESS_TERMCAP_se (set_color normal)
+setenv -x LESS_TERMCAP_so (set_color -b blue -o yellow)
+setenv -x LESS_TERMCAP_ue (set_color normal)
+setenv -x LESS_TERMCAP_us (set_color -o green)
+
+# use a daemon for emacs
 alias e="emacsclient -c -t"
 alias emacs="emacsclient -c"
+alias proc="ps -aux| grep"
